fa25dc587db91e03ce840a691e665e90ee443e3c
choihalim <halim331502@gmail.com>
2024-07-14 15:18:08 +0900

    feat: CSV Reader 구현

---
+++ src/main/java/com/kane/calendarapp/reader/EventCsvReader.java
@@ -0,0 +1,193 @@
+package com.kane.calendarapp.reader;
+
+import com.kane.calendarapp.event.Meeting;
+import com.kane.calendarapp.event.NoDisturbance;
+import com.kane.calendarapp.event.OutOfOffice;
+import com.kane.calendarapp.event.Todo;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class EventCsvReader {
+
+    private final RawCsvReader rawCsvReader;
+
+    public EventCsvReader(RawCsvReader rawCsvReader) {
+        this.rawCsvReader = rawCsvReader;
+    }
+
+    public List<Meeting> readMeetings(String path) throws IOException {
+        List<Meeting> result = new ArrayList<>();
+
+        //데이터를 읽는 부분
+        List<String[]> read = rawCsvReader.readAll(path);
+        for (int i = 0; i < read.size(); i++) {
+            if (skipHeader(i)) {
+                continue;
+            }
+
+            String[] each = read.get(i);
+
+            result.add(
+                    new Meeting(
+                            Integer.parseInt(each[0]),
+                            each[2],
+                            ZonedDateTime.of(
+                                    LocalDateTime.parse(
+                                            each[6],
+                                            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                                    ),
+
+                                    ZoneId.of("Asia/Seoul")
+                            ),
+                            ZonedDateTime.of(
+                                    LocalDateTime.parse(
+                                            each[7],
+                                            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                                    ),
+
+                                    ZoneId.of("Asia/Seoul")
+                            ),
+                            new HashSet<>(Arrays.asList(each[3].split(","))),
+                            each[4],
+                            each[5]
+                    )
+            );
+        }
+
+
+        return result;
+    }
+
+    public List<NoDisturbance> readNoDisturbance(String path) throws IOException {
+        List<NoDisturbance> result = new ArrayList<>();
+
+        //데이터를 읽는 부분
+        List<String[]> read = rawCsvReader.readAll(path);
+        for (int i = 0; i < read.size(); i++) {
+            if (skipHeader(i)) {
+                continue;
+            }
+
+            String[] each = read.get(i);
+
+            result.add(
+                    new NoDisturbance(
+                            Integer.parseInt(each[0]),
+                            each[2],
+                            ZonedDateTime.of(
+                                    LocalDateTime.parse(
+                                            each[3],
+                                            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                                    ),
+
+                                    ZoneId.of("Asia/Seoul")
+                            ),
+                            ZonedDateTime.of(
+                                    LocalDateTime.parse(
+                                            each[4],
+                                            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                                    ),
+
+                                    ZoneId.of("Asia/Seoul")
+                            )
+
+                    )
+            );
+        }
+        return result;
+    }
+
+    public List<OutOfOffice> readOutOfOffice(String path) throws IOException {
+        List<OutOfOffice> result = new ArrayList<>();
+
+        //데이터를 읽는 부분
+        List<String[]> read = rawCsvReader.readAll(path);
+        for (int i = 0; i < read.size(); i++) {
+            if (skipHeader(i)) {
+                continue;
+            }
+
+            String[] each = read.get(i);
+
+            result.add(
+                    new OutOfOffice(
+                            Integer.parseInt(each[0]),
+                            each[2],
+                            ZonedDateTime.of(
+                                    LocalDateTime.parse(
+                                            each[3],
+                                            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                                    ),
+
+                                    ZoneId.of("Asia/Seoul")
+                            ),
+                            ZonedDateTime.of(
+                                    LocalDateTime.parse(
+                                            each[4],
+                                            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                                    ),
+
+                                    ZoneId.of("Asia/Seoul")
+                            )
+
+                    )
+            );
+        }
+        return result;
+    }
+
+    public List<Todo> readTodo(String path) throws IOException {
+        List<Todo> result = new ArrayList<>();
+
+        //데이터를 읽는 부분
+        List<String[]> read = rawCsvReader.readAll(path);
+        for (int i = 0; i < read.size(); i++) {
+            if (skipHeader(i)) {
+                continue;
+            }
+
+            String[] each = read.get(i);
+
+            result.add(
+                    new Todo(
+                            Integer.parseInt(each[0]),
+                            each[2],
+                            ZonedDateTime.of(
+                                    LocalDateTime.parse(
+                                            each[4],
+                                            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                                    ),
+
+                                    ZoneId.of("Asia/Seoul")
+                            ),
+                            ZonedDateTime.of(
+                                    LocalDateTime.parse(
+                                            each[5],
+                                            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                                    ),
+
+                                    ZoneId.of("Asia/Seoul")
+                            ),
+                            each[3]
+                    )
+            );
+        }
+
+        return result;
+
+    }
+
+    private boolean skipHeader(int i) {
+        return i == 0;
+    }
+}
+
+
+
